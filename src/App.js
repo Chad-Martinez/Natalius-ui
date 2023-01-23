a53f0af893b2ffb61460cc2a1f0261013e01e0b1
Chad Martinez <chadcito@Chads-Mac-Studio.local>
2023-01-23 09:37:39 -0700

    added logout functionality

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
       <Layout>{routes}</Layout>
       <ToastContainer
         position='top-right'
-        autoClose={5000}
+        autoClose={3000}
         hideProgressBar={false}
         newestOnTop={false}
         closeOnClick
